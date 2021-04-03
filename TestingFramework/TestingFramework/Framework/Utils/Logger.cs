4afb710943532d83118a76b1c585f9ecca1af4d7
Artyom Gordeychiik <dogrti100@gmail.com>
2021-04-03 15:33:12 +0300

    Added empty utils classes

--- TestingFramework/TestingFramework/Framework/Utils/Logger.cs
+++ TestingFramework/TestingFramework/Framework/Utils/Logger.cs
@@ -1,10 +1,34 @@
-﻿using System;
+﻿using log4net;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace TestingFramework.Framework.Utils
 {
-    class Logger
+    public class Logger
     {
+        public static ILog Log { get; } = LogManager.GetLogger("LOGGER");
+
+        public static void InitLogger()
+        {
+            XmlConfigurator.Configure();
+        }
+
+        public static void MakeStepLog(int numberOfStep, string step)
+        {
+            Logger.Log.Info(numberOfStep + " - " + step);
+        }
+
+        public static void MakeLog(string log)
+        {
+            Logger.Log.Info(log);
+        }
+
+        public static void MakeErrorLog(string message)
+        {
+            string customLog = "Error: " + message;
+            Logger.Log.Info(customLog);
+            throw new System.Exception();
+        }
     }
 }
